8ce51336bc7666f72ed373e30788111acb52ca21
Dmitry Kolupaev <dmitry.klpv@gmail.com>
2018-04-09 00:00:03 +0300

    Add spec for customClass property

--- src/specs/property-config.spec.ts
+++ src/specs/property-config.spec.ts
@@ -27,6 +27,38 @@ describe('Property config: key', () => {
 
 });
 
-// TODO: spec for customClass
+class PropertyClass {
+
+  @Property()
+  public innerProperty: string;
+}
+class SimpleClassWithCustomClassKey {
+
+  @Property({ customClass: PropertyClass })
+  public propertyClass;
+}
+
+describe('Property config: customClass', () => {
+
+  it('creates a child property class with specified constructor', () => {
+    const raw = { propertyClass: { innerProperty: 'Test' } };
+    const converted = fromJson<SimpleClassWithCustomClassKey>(typeInfo(SimpleClassWithCustomClassKey), raw);
+    expect(converted.propertyClass.constructor).to.equal(PropertyClass);
+  });
+
+  it('creates a child property class with property in it filled', () => {
+    const raw = { propertyClass: { innerProperty: 'Test' } };
+    const converted = fromJson<SimpleClassWithCustomClassKey>(typeInfo(SimpleClassWithCustomClassKey), raw);
+    expect(converted.propertyClass.innerProperty).to.equal('Test');
+  });
+
+  it('throws an error if incoming key for inner property class doesnt exist', () => {
+    const raw = { propertyClass: { innerPropertyZ: 'Test' } };
+    const testFunc = () => fromJson<SimpleClassWithCustomClassKey>(typeInfo(SimpleClassWithCustomClassKey), raw);
+    expect(testFunc).to.throw('[FromJson]: Unknown property passed: innerPropertyZ');
+  });
+
+});
+
 // TODO: spec for generic
 // TODO: spec for customClass when generic: true
