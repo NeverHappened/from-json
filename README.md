f0b59699db68b578487a957292677da69d57ac0d
Dmitry Kolupaev <dmitry.klpv@gmail.com>
2018-04-09 21:03:26 +0300

    Add specs for array convertion. Improve README

--- README.md
+++ README.md
@@ -2,29 +2,39 @@
 
 
 # Usage
-Before usage you must import 'reflect-metadata' package into your project
+Before usage you must import 'reflect-metadata' package into your project.
+It's global, so you can add it in a separate script. For example, in polyfills.ts for Angular 2+.
 ```
 import 'reflect-metadata';
 ```
-Also you need to set ```"emitDecoratorMetadata": true``` in your tsconfig.json to autodiscover feature to work
+Also you need to set ```"emitDecoratorMetadata": true``` in your tsconfig.json to autodiscover feature and array detection to work
+
+
+# Gotchas
+1. You cannot use autodiscover if you have an array, because we cannot infer the generic constructor from the Array class. Use customClass.
+2. You should use autodiscover to automatically convert classes with @Property inside
+3. If you don't want to convert into the class (It's simple numbers/strings/interface), just don't use anything at all
+4. We don't need a customClass if using ```` { generic: true } ````, because it'll be taken from the ```TypeInfo``` passed in ```fromJson``` function
 
 
 # Development
-
 
 Install dependencies
 ```
 yarn
 ```
 
-
 Compile typescript
 ```
 yarn run compile
 ```
 
-
 Publish to npm
 ```
 npm publish
+```
+
+Run tests
+```
+npm run test
 ```
--- src/specs/property-array.spec.ts
+++ src/specs/property-array.spec.ts
@@ -1,1 +1,82 @@
-// TODO
+import { expect } from 'chai';
+import 'mocha';
+import 'reflect-metadata';
+import { Property, fromJson, typeInfo as ty } from '../public-api';
+
+class ArraySimpleClass {
+  @Property()
+  public array: number[];
+}
+
+describe('Array keys', () => {
+
+  it('should correctly initialize the array of simple values', () => {
+    const array = [1, 2, 3, 9];
+    const raw = { array };
+    const result = fromJson<ArraySimpleClass>(ty(ArraySimpleClass), raw);
+    expect(result.array).to.eq(array);
+    expect(result.array.length).to.eq(array.length);
+  });
+});
+
+class PropertyClass {
+  @Property()
+  public prop: string;
+
+  public test(): number { return 4; }
+}
+class ArrayCustomClass {
+  @Property({ customClass: PropertyClass })
+  public array: PropertyClass[];
+}
+
+describe('Array keys with custom classes', () => {
+
+  it('should correctly initialize the array of custom class values', () => {
+    const array = [ { prop: 'prop1' }, { prop: 'prop2' }];
+    const raw = { array };
+    const result = fromJson<ArrayCustomClass>(ty(ArrayCustomClass), raw);
+    expect(result.array).to.not.eq(array);
+    expect(result.array.length).to.eq(array.length);
+  });
+
+  it('should correctly initialize custom class items in array', () => {
+    const array = [ { prop: 'prop1' }, { prop: 'prop2' }];
+    const raw = { array };
+    const result = fromJson<ArrayCustomClass>(ty(ArrayCustomClass), raw);
+    const firstItem = result.array[0];
+    expect(firstItem.constructor).to.eq(PropertyClass);
+    expect(firstItem.test()).to.eq(4);
+    expect(firstItem.prop).to.eq('prop1');
+  });
+});
+
+class ConcretePropertyItemClass {
+  @Property()
+  public prop: string;
+
+  public test(): number { return 4; }
+}
+class ArrayGenericClass<T> {
+  @Property({ generic: true })
+  public array: T[];
+}
+
+describe('Array keys with generic values', () => {
+  const typeInfo = ty(ArrayGenericClass, { array: ty(ConcretePropertyItemClass) });
+  const raw = { array: [ { prop: 'prop1' }, { prop: 'prop2' } ] };
+
+  it('should correctly create array with passed type info', () => {
+    const result = fromJson<ArrayGenericClass<ConcretePropertyItemClass>>(typeInfo, raw);
+    expect(result.constructor).to.eq(ArrayGenericClass);
+    expect(result.array.constructor).to.eq(Array);
+  });
+
+  it('should initialize items of correct concrete types', () => {
+    const result = fromJson<ArrayGenericClass<ConcretePropertyItemClass>>(typeInfo, raw);
+    const firstItem = result.array[0];
+    expect(firstItem.constructor).to.eq(ConcretePropertyItemClass);
+    expect(firstItem.test()).to.eq(4);
+    expect(firstItem.prop).to.eq('prop1');
+  });
+});
